2dee30906a21654c57a384cda30243ea7a9cdb6f
Shamaila99 <shamailaafridi92@gmail.com>
2015-06-04 12:19:57 +0100

    edited errors and saved new files

--- Cat.java
+++ Cat.java
@@ -1,8 +1,8 @@
 public class Cat{
 	public static void main(String[] args){
 		String cat = ("Maine Coon");
-		int growth = 4
-		System.out.println("The " + cat + " can grow for up to " + growth " years.");
+		int ageOfCat = 4;
+		System.out.println("The " + (cat) + " can grow for up to " + (ageOfCat) + " years");
 
 	}
 }
